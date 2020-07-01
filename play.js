5f300fe8c7f9f3219b40ec741396c84bc2ffe266
Manvi Kapoor <kapoor.manvi11@gmail.com>
2020-07-01 22:29:25 +0000

    refactored listeners to an input file

--- play.js
+++ play.js
@@ -5,27 +5,12 @@ const net = require('net');
  */
 const { connect } = require('./client');
 console.log('Connecting ...');
+const { setupInput } = require('./input');
+
 console.log(connect);
 connect();
 
-const handleUserInput = function(key) {
-  console.log("key: ", key);
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
 setupInput();
+
+
+
