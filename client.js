bfd3431c21b555e6245bc927132bffe01ac83f0a
Manvi Kapoor <kapoor.manvi11@gmail.com>
2020-07-01 22:03:30 +0000

    the snake moves up using setInterval

--- client.js
+++ client.js
@@ -13,7 +13,29 @@ const connect = () => {
   
   conn.on("connect", () => {
     console.log("Successfully connected to server");
-    conn.write("Name: MK");  
+    conn.write("Name: MK"); 
+    
+    let count = 0;
+    let moveUp = setInterval(() => {
+      process.stdout.write(conn.write("Move: up"));
+      count++;
+      if (count > 5) {
+        clearInterval (moveUp);
+      }
+    }, 300);
+    
+  
+    // setTimeout(() => {
+    //   process.stdout.write(conn.write("Move: up"));
+    // }, 400);
+
+    // setTimeout(() => {
+    //   process.stdout.write(conn.write("Move: up"));
+    // }, 700);
+
+   
+    // conn.write("Move: up"); 
+   
   });
 
   conn.on("data", (data) => {
